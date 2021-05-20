3079d5f2a351e08591348a60afbda3f19df54468
sabre <djsoundnr1@gmail.com>
2021-05-20 13:14:13 +0200

    Initial commit

---
+++ src/main/java/projekt/ts/authservice/AuthService.java
@@ -0,0 +1,12 @@
+package projekt.ts.authservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthService {
+
+    public boolean authenticateUser(String email, String passwordhash) {
+
+    }
+    // TODO: możemy zrobić te maile może?
+}
